9f945af9f979cb114982a17158a63c98954705fe
crg <botsandpods@gmail.com>
2025-03-14 21:58:35 +0100

    initiat commit

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import Board from '@/components/Board';
 
 export default function Home() {
   return (
-    <main className="p-6">
+    <main className="p-6 bg-black text-white font-mono min-h-screen">
       <h1 className="text-2xl font-bold">Juego BombMin3</h1>
       <ConnectWallet />
       <TokenValue />
