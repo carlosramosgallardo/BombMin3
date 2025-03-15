f54beacd0ba995eca6782019ce43e22521ec941a
crg <botsandpods@gmail.com>
2025-03-15 03:44:37 +0100

    initiat commit

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,11 @@
 import ConnectWallet from '@/components/ConnectWallet';
 import TokenValue from '@/components/TokenValue';
 import Board from '@/components/Board';
+import '@/app/globals.css';
 
 export default function Home() {
   return (
-    <main className="p-6 bg-black text-white font-mono min-h-screen flex flex-col items-center justify-center text-lg">
+    <main className="p-6 min-h-screen flex flex-col items-center justify-center text-lg">
       <h1 className="text-4xl font-bold mb-6">BombMin3</h1>
       <ConnectWallet />
       <TokenValue />
